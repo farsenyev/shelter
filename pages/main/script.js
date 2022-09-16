66168d9ea196e0fd99ee453f3aaa6256d4ac7ff5
farsenyev <farsenyev@bk.ru>
2022-09-16 07:15:03 +0300

    feat: create .gitinore and some block in html files

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,1 +1,11 @@
 
+function showModal () {
+    document.getElementById('modal-jenifer').style.visibility = 'visible'
+    document.getElementById('shadow-block').style.visibility = 'visible'
+    document.body.style.background = ' rgba(41, 41, 41, 0.6);'
+    console.log('cong')
+}
+function hideModal () {
+    document.getElementById('modal-jenifer').style.visibility = 'hidden'
+    document.getElementById('shadow-block').style.visibility = 'hidden'
+}
